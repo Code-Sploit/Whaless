4194392cb4f17f2a7f2a8ce0e6dab7eb029fe3fb
SolindekDev <solindeklive.biznes@gmail.com>
2023-02-09 18:25:32 +0100

    Added some pieces

--- app.c
+++ app.c
@@ -5,7 +5,8 @@
 #include <draw.h>
 #include <config.h>
 
-static struct __game_state* game = NULL;
+static struct __game_state* game       = NULL;
+static unsigned char table_color[8][8] = BOARD_COLORS;
 
 void app_start(__window_t* window)
 {
@@ -18,14 +19,12 @@ void draw_table_chess(__window_t* window)
     {
         for (int x = 0; x < 8; x++)
         {
-            __color_t bg_color = game->__board[y][x].__player == PLAYER_WHITE
+            __color_t bg_color = table_color[y][x] == 0
                                   ? (__color_t){ LIGHT_SQUARE_COLOR }
                                   : (__color_t){ DARK_SQUARE_COLOR  };
-            printf("%d ", game->__board[y][x].__player);
             draw_rect(window, vec2(y * BOARD_SQUARE_SIZE, x * BOARD_SQUARE_SIZE),
                       vec2(BOARD_SQUARE_SIZE, BOARD_SQUARE_SIZE), bg_color);
         }
-        printf("\n");
     }
 
 }
